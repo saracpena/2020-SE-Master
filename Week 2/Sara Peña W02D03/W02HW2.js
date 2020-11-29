5c90f232ded23f2123bebfb97278ace8921164c0
Sara Peña <s.cristal.p@gmail.com>
2020-11-29 14:21:35 -0500

    W2H2 continued

--- Week 2/Sara Peña W02D03/W02HW2.js
+++ Week 2/Sara Peña W02D03/W02HW2.js
@@ -2,57 +2,100 @@
 
 //1. Write a function maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. 
 //use the if-then-else construct available in Javascript. Do some googling to figure this out if you forget how conditionals work.
-console.log("       Number 1======================")
+// console.log("       Number 1======================")
 
-let num1 = 22
-let num2 = 21
+// function maxOfTwoNumbers (num1,num2){
+//     if(num1>num2){
+//         console.log(num1);
+//     } else {
+//         console.log(num2);
+//     }
+// }     
+// maxOfTwoNumbers(22,21);
 
-function maxOfTwoNumbers (num1,num2){
-    if(num1>num2){
-        console.log(num1);
-    } else if (num1<num2){
-        console.log(num2);
-    }
-}     
-maxOfTwoNumbers(num1,num2);
+// // function maxOfTwoNumbers(num1,num2){
+// //     (num1 > num2) ? console.log(num1) : console.log(num2); //ternary statement
+// // }
+// // max0fTwoNumbers(22,21);
 
-//2. Write a function maxOfThree that takes three numbers as arguments and returns the largest of them.
-console.log('       NUMBER 2=======================');    
+// //2. Write a function maxOfThree that takes three numbers as arguments and returns the largest of them.
+// console.log('       NUMBER 2=======================');    
     
-function maxOfThree (x,y,z){
-    if ((x > y) && (x > z)) { //boolean
-        return x;
-    } else if ((y > x) && (y > z)) { //boolean
-        return y;
-    } else {
-        return z;
-    }
-}
-    console.log(maxOfThree(20,28,35));
-    
+// function maxOfThree (x,y,z){
+//     if ((x > y) && (x > z)) { //boolean
+//         return x;
+//     } else if ((y > x) && (y > z)) { //boolean
+//         return y;
+//     } else {
+//         return z;
+//     }
+// }
+//     console.log(maxOfThree(20,28,35));
+
+//SO MUCH SIMPLER
+// function maxOfThreeNumbers(num1, num2, num3) {
+//     if(num1 > num2 && num1 > num3) {
+//         console.log(num1);
+//     } else if (num2 > num3 && num2 > num1) {
+//         console.log(num2);
+//     } else { 
+//         console.log(num3);
+//     }
+// }
+//     maxOfThreeNumbers(1000, 20, 10);
 
 //3. Write a function isCharacterAVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise
-console.log("       NUMBER 3==========================");
+// console.log("       NUMBER 3==========================");
 
-    function isCharacterAVowel(vowels){
-        if(vowels === 'a'||'e'||'i'||'o'||'u'){
-            return "The character is a vowel";
-        }
-        else{
-            return "The character is a consanant";
-        }
+function isCharacterAVowel(vowels){
+    if(vowels === 'a'||vowels === 'e'||vowels === 'i' || vowels === 'o' || vowels === 'u'){
+        return "The character " + vowels + " is a vowel";
     }
-        console.log(isCharacterAVowel("a"));
+    else{
+        return "The character " + vowels + " is NOT a vowel" ;
+    }
+}
+    console.log(isCharacterAVowel("z"));
+
+//2nd Version
+// var vowels = ['a','e','i','o','u'];
+
+//     function isCharacterAVowel(vowels,x){ //define function
+//         var newVowel = vowels.filter(elements => elements === x);
+//         return newVowel.length > 0 ? "The character " + x + " is a vowel": "The character " + x + " is NOT a vowel" ;
+// }
+// console.log(isCharacterAVowel(vowels,'y'));
 
 //4. Define a function sumArray and a function multiplyArray that sums and multiplies (respectively) 
 //all the numbers in an array of numbers. For example, sumArray([1,2,3,4]) should return 10, and 
 //multiplyArray([1,2,3,4]) should return 24.
-        
+
+var result = 0;
+
+function sumArray(x){
+    x.forEach(element => {
+        result+= elements; 
+    });
+    return result;
+}
+// function multiplyArray()
+
 
 //5. Write a function that returns the number of arguments passed to the function when called.
 
 //6. Define a function reverseString that computes the reversal of a string. For example, reverseString("jag testar") 
 //should return the string "ratset gaj".
+
+// let revString = (str) => {
+//     //1. step 1 use split("") on str to split the characters and return a new array.
+//     var splitArray = str.split("");
+//     //2. Now reverse this new array "splitArray" using reverse();
+//     var reverseArray = splitArray.reverse();
+//     //3. Now use join() to join the individual characters in "reverseArray" to make the whole string and output the result
+//     console.log("The original string " + str + " is reversed to " + reverseArray.join(""));
+//   }
+//   var strArg = "!World is Beautiful!";
+//   revString(strArg);
         
 //7. Write a function findLongestWord that takes an array of words and returns the length of the longest one.
         
@@ -63,6 +106,8 @@ console.log("       NUMBER 3==========================");
         
 //1. Add a method reverseString (from question 6) to the object String so that it is possible to call: 
 //"Per Scholas".reverseString().
+
+
         
 //2. Write a function that takes a string as argument and returns an object where:
     // a. the keys are the characters that occur in the string
