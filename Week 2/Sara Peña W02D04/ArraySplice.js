b6b6de1d80d0adcd5482622fd69554b9b9f2cd4c
Sara Peña <s.cristal.p@gmail.com>
2020-12-03 14:33:03 -0500

    ArraySplice

---
+++ Week 2/Sara Peña W02D04/ArraySplice.js
@@ -0,0 +1,13 @@
+var arr = ['a','b','c','d'];
+var str = "Welcome to Software Engineering Class";
+
+//SYNTAX array.splice(start, [delete count], [item], [item2])
+// console.log(arr.splice(1,2,'X','Y','W'));
+// console.log(arr);
+
+//array.slice(begin, end)
+//string.slice(begin, end)
+console.log(arr.slice(1,2));
+console.log(str.slice(1,2));
+console.log(arr);
+console.log(str);
