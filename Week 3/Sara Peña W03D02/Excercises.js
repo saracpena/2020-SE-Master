303329c9b01ebe61b36a93020b35c3b755d8847c
Sara Peña <s.cristal.p@gmail.com>
2020-12-09 12:11:32 -0500

    Excercises.js

---
+++ Week 3/Sara Peña W03D02/Excercises.js
@@ -0,0 +1,10 @@
+// 1. Create an Array containing 10 values
+// 2. Create an empty Array
+// 3. Use for loop to iterate thru the array (start @ index 0)
+// 4. Assign a value to every index while iterating array
+
+var myArr = [];
+for(var i = 0; i <= 10; i++){
+    myArr[i] = i;
+}
+console.log(myArr);//i is a value for each element in the array
