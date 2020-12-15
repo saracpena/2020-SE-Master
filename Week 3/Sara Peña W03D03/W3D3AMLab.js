3c658632edf4b83470765c35d21cace4e0b109ad
Sara Peña <s.cristal.p@gmail.com>
2020-12-15 08:42:26 -0500

    Update W3D3AMLab.js

--- Week 3/Sara Peña W03D03/W3D3AMLab.js
+++ Week 3/Sara Peña W03D03/W3D3AMLab.js
@@ -11,10 +11,10 @@
 
 var VendingMachine = {
     snacks: [
-        {name:"Smarties", code:"A01", quantity:10, price:0.60},
-        {name:"Caramac Bar", code:"A02", quantity:5, price:0.60},
-        {name:"Dairy Milk", code:"A03", quantity:1, price:0.65},
-        {name:"Freddo", code:"A04", quantity:1, price:0.25}],
+        {name:"Crunch", code:"A01", quantity:10, price:0.60},
+        {name:"Milky Way", code:"A02", quantity:5, price:0.60},
+        {name:"Vanilla Cold Brew", code:"A03", quantity:1, price:0.65},
+        {name:"Honest Honey Green Tea", code:"A04", quantity:1, price:0.25}],
 
 
  vend: function(indexPosition){
