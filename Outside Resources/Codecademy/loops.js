b043e22e52a8ac07fa7fa112b281b7f2b0af93cb
Sara Peña <s.cristal.p@gmail.com>
2020-12-16 23:26:59 -0500

    Update loops.js

---
+++ Outside Resources/Codecademy/loops.js
@@ -0,0 +1,16 @@
+//In this 'FOR' loop, the instructions run 10 times.
+for (i = 0; i < 10; i++) {//Go straight to creating 'for'
+    placeTile('mint')
+    placeTile('orange')
+    placeTile('mint')
+  }
+
+//In this 'WHILE loop, we create a counter at 0 and add 1 to it every iteration. The loop is executed until the counter is 10 or more.
+let i = 0;//begin by creating a variable 'let' then follow up with a 'while'
+while (i < 10) {
+  placeTile('mint')
+  placeTile('orange')
+  placeTile('mint')
+  i++
+}
+
