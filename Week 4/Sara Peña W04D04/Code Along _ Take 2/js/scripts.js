b3f469958fc1d9dfc739cffe30d5df2ad405d3bc
Sara Peña <s.cristal.p@gmail.com>
2020-12-22 08:37:09 -0800

    WK4D4.js

---
+++ Week 4/Sara Peña W04D04/Code Along _ Take 2/js/scripts.js
@@ -0,0 +1,65 @@
+// $('#input-box').val()
+
+// $('#submit-btn').on('click' , () => {
+//     console.log($('#input-box').val());//show value of what is input on text box
+// })
+
+//VERSES NEXT CODE:
+$('form').on('submit', (event) => {//I removed id = submit-btn
+    console.log($('#input-box').val());//show value of what is input on text box
+    event.preventDefault();
+})
+
+
+
+
+// console.log('stranger things are coming');
+
+// $(()=> {
+//     const addH2 = () => {
+//         let $h2 = $('<h2>').text("Just getting started");
+//         $('body').append($h2);
+//     }
+
+// addH2();
+// });
+
+// // const promise = $.ajax({
+// //     url: 'http://www.omdbapi.com/apikeu=53aa2cd6&t=Frozen',
+// // });
+   
+// // promise.then(
+// //     (data)=>{
+// //         console.log(data);
+// //     },
+// //     ()=>{
+// //        console.log("bad request");
+// //     }
+// // );
+
+// //API is like a username/password
+// //AJAX code currently not working 12/18 @ 10:45am
+
+// // let key = '53aa2cd6'
+// // let movieTitle = 'Mad Max: Fury Road'
+// // $(() => {
+// //     $('form').on('submit', (event) => {
+// //         event.preventDefault()
+// //         const userInput = $('input[type="text"]').val()
+// //         const promise = $.ajax({
+// //             url: `http://www.omdbapi.com/?apikey=${key}&t=${userInput}`
+// //         })
+        
+// //         promise.then(
+// //             (data) => {
+// //                 console.log(data)
+// //                 $('#title').html(data.Title)
+// //                 $('#year').html(data.Year)
+// //                 $('#rated').html(data.Rated)
+// //             },
+// //             () => {
+// //                 console.log('bad request')
+// //             }
+// //         )
+// //     })
+// // })
