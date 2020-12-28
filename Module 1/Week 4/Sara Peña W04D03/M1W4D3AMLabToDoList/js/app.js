97e43d6a8eb05c6ce2aac90093048e103b738be9
Sara Peña <s.cristal.p@gmail.com>
2020-12-28 14:35:17 -0800

    Space Battle Beginnings

--- Module 1/Week 4/Sara Peña W04D03/M1W4D3AMLabToDoList/js/app.js
+++ Module 1/Week 4/Sara Peña W04D03/M1W4D3AMLabToDoList/js/app.js
@@ -1,61 +1,110 @@
+// key listener reference: https://www.techiedelight.com/detect-enter-key-press-javascript/#:~:text=In%20plain%20JavaScript%2C%20you%20can,an%20Enter%20key%20is%20pressed.
 
-// const toDos = [];
-// const done = [];
+$(() => {
 
-// const completeButton = "<input type = 'submit' value = 'COMPLETED'>;
-// const removeButton = "<input type = ''
+	// add item on press enter
+	$('#task').on('keydown',(e)=>{
+		if (e.which === 13) {
+			$('#add').click();
+		}
+	});
+	// adding items to the list
+	$("#add").on("click",()=>{
+		// get the input
+		const newItem = $("#task").val();
+		//clear the input
+		$('#task').val("");
+		// add it to the todo list
+		const $newListItem = $('<li>');
+		$newListItem.addClass('to-do-item');
+		$newListItem.addClass('list-item');
+		$newListItem.append($('<span>').text(newItem));
+		// $newListItem.text(newItem);
+		// add a button to mark as done as well
+		const $doneButton = $('<button>');
+		$doneButton.addClass('list-item-control');
+		$doneButton.text('Mark Done');
+		// consider only having a single event handler on the whole list
+		// https://ehsangazar.com/optimizing-javascript-event-listeners-for-performance-e28406ad406c
+		$doneButton.on('click', ()=>{
+			$newListItem.detach().appendTo($('#done-list'));
+			$newListItem.removeClass('to-do-item');
+			$newListItem.addClass('done-item');
 
-// $('form').on('#add', (e) => {
-//   console.log($('#task').val());
+			$doneButton.text('Remove Item');
+			// doing this inside really seems wrong
+			// I could remove the original button and add a new one  
+			$doneButton.on('click', ()=>{
+				$newListItem.remove();
+			});
+		});
 
-//   todos.push($('#task').val());
-//   // list.push($('#input-box-mn').val());
-//   // list.push($('#input-box-ln').val());
-//   console.log(todos); //test it
-//   e.preventDefault();
+		$newListItem.append($doneButton);
 
-//   $(e.currentTarget).trigger('reset');
-//   render();
-// });
-// const render = () => { //prints the item
-//   // console.log('list:', list);
-//   // $('ul').empty();
-//   // list.forEach((item) => { //it iterates the array
-//   //     console.log(item);
-//   $('ul').append('<li>' + todos[todos.length - 1] + '</li>');
-//   // });
-//   $('li').on('click', (e) => {
+		$('#to-do-list').append($newListItem);
 
-//       $(e.currentTarget).css('text-decoration', 'line-through');
-//       $(e.currentTarget).effect('shake');
+	});
+
+});
+
+
+// // const toDos = [];
+// // const done = [];
+
+// // const completeButton = "<input type = 'submit' value = 'COMPLETED'>;
+// // const removeButton = "<input type = ''
+
+// // $('form').on('#add', (e) => {
+// //   console.log($('#task').val());
+
+// //   todos.push($('#task').val());
+// //   // list.push($('#input-box-mn').val());
+// //   // list.push($('#input-box-ln').val());
+// //   console.log(todos); //test it
+// //   e.preventDefault();
+
+// //   $(e.currentTarget).trigger('reset');
+// //   render();
+// // });
+// // const render = () => { //prints the item
+// //   // console.log('list:', list);
+// //   // $('ul').empty();
+// //   // list.forEach((item) => { //it iterates the array
+// //   //     console.log(item);
+// //   $('ul').append('<li>' + todos[todos.length - 1] + '</li>');
+// //   // });
+// //   $('li').on('click', (e) => {
+
+// //       $(e.currentTarget).css('text-decoration', 'line-through');
+// //       $(e.currentTarget).effect('shake');
+// //   });
+
+// $(function() {
+//   $("#add").on("click", function() {
+//       var val = $("input").val();
+//       if (val !== "") {
+//           var elem = $("<li></li>").text(val);
+//           $(elem).append("<button class='rem'>Completed</button>");
+//           $("#mylist").append(elem);
+
+//           $("input").val("");
+//           $(".rem").on("click", function() {
+//             for(let i=0; i<=10; i++){
+//               var j = $("li").eq().remove();//Number($('li:eq(i)'));
+//               //console.log(j);
+//             }
+//             var j = $("li").eq(1);//$('li:eq(2)');
+//             console.log(j);
+//             for(let i = 0; i <= i.length; i++){
+//               $('rem').parent().remove(eq[j]);
+//             }
+//           });
+//       }
+//       $('.rem').on('click', function(){
+//         $('#mylist-1').append(elem);
+//         $(elem).append("<button class='item'>Remove</button>");
+//         $('.rem').remove();
+//         $('li').css('backgroundColor', '#ED6495');
+//       })
+//     });
 //   });
-
-$(function() {
-  $("#add").on("click", function() {
-      var val = $("input").val();
-      if (val !== "") {
-          var elem = $("<li></li>").text(val);
-          $(elem).append("<button class='rem'>Completed</button>");
-          $("#mylist").append(elem);
-
-          $("input").val("");
-          $(".rem").on("click", function() {
-            for(let i=0; i<=10; i++){
-              var j = $("li").eq().remove();//Number($('li:eq(i)'));
-              //console.log(j);
-            }
-            var j = $("li").eq(1);//$('li:eq(2)');
-            console.log(j);
-            for(let i = 0; i <= i.length; i++){
-              $('rem').parent().remove(eq[j]);
-            }
-          });
-      }
-      $('.rem').on('click', function(){
-        $('#mylist-1').append(elem);
-        $(elem).append("<button class='item'>Remove</button>");
-        $('.rem').remove();
-        $('li').css('backgroundColor', '#ED6495');
-      })
-    });
-  });
---
+++ Projects/Mod-1 Space Battle/js/app.js
@@ -0,0 +1,60 @@
+//!You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
+//!Battle the aliens as you try to destroy them with your lasers.
+
+//!There are six alien ships
+//!Aliens' weakness is that they are too logical and attack one at a time
+//!they will wait to see the outcome of a battle before deploying another alien ship.
+
+//!Your strength is that you have the initiative and get to attack first. 
+//!However, you do not have targeting lasers and can only attack the aliens in order. 
+//!After you have destroyed a ship, you have the option to make a hasty retreat.
+//* BEGIN CODING HERE *//
+
+//* A game round would look like this:
+//! LOOP
+//TODO: Attack the first alien ship, 
+//TODO: If the ship survives, it attacks you
+//TODO: If you survive, you attack the ship again
+
+//! DESTROYED
+//TODO: If you destroy the ship, you have the option to attack the next ship or to retreat
+//TODO: If you retreat, the game is over, perhaps leaving the game open for further developments or options.
+//TODO: You win the game if you destroy all of the aliens.
+//?You lose the game if you are destroyed.
+
+//* SHIP PROPERTIES
+//TODO: 'hull' is the same as hitpoints. If hull reaches 0 or less, the ship is destroyed.
+    let hull;
+    for(hull = 0; hull<=0; hull++){
+
+    }
+
+//TODO: 'firepower' is the amount of damage done to the 'hull' of the target with a successful hit.
+
+//TODO: 'accuracy' is the chance between 0 and 1 that the ship will hit its target.
+
+//! SPACE SHIP BUILD//
+//TODO: 'hull' - 20
+//TODO: 'firepower' - 5
+//TODO: 'accuracy' - .7
+
+//! ALIEN SHIP BUILD
+//TODO: 'hull' -  between 3 and 6 
+//TODO: 'firepower' - between 2 and 4
+//TODO: 'accuracy' -  between .6 and .8
+
+//!PLAYER
+    //!ATTACK
+//!VILLAIN
+    //!ATTACK
+
+
+
+/**
+ * MyMethod
+ * * Important
+ * ! Dep method
+ * ?Should?
+ * TODO:
+*/
+
