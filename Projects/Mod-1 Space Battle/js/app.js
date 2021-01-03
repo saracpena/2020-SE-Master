6f477c21ea0a6a9a0211405fb89b37c8fa9ac9ad
Sara Peña <s.cristal.p@gmail.com>
2021-01-03 15:58:45 -0500

    SpaceBattle updates

--- Projects/Mod-1 Space Battle/js/app.js
+++ Projects/Mod-1 Space Battle/js/app.js
@@ -21,7 +21,7 @@
 //TODO: You win the game if you destroy all of the aliens.
 //?You lose the game if you are destroyed.
 
-//* SHIP PROPERTIES
+//* SHIP + ALIEN PROPERTIES
 //TODO: 'hull' is the same as hitpoints. If hull reaches 0 or less, the ship is destroyed.
 //?LOOP? i.e.
     // let hull;
@@ -33,107 +33,84 @@
 //TODO: 'hull' - 20 //the same as hitpoints. If hull reaches 0 or less, the ship is destroyed.
 //TODO: 'firepower' - 5 //the amount of damage done to the 'hull' of the target with a successful hit.
 //TODO: 'accuracy' - .7 //the chance between 0 and 1 that the ship will hit its target.
-// CREATE CAPTAIN OBJECT
 //!Math.random() - returns a floating-point, pseudo-random number in the range 0 to less than 1, which you can then scale to your desired range
-//console.log('window is loaded');
+
 
 //* BEGIN CODING HERE *//
 
-//
+console.log('%c Welcome to Space Battle, Captain!','font-size: 20px; font-style: italic; background: azure; border: 1px solid grey;')
+
 setTimeout(function() {
     alert(
       "Welcome to Sara's Space Battle page! - open the console log on the page to play. Hurry the fate of the planet is in your hands Captain!"
     );
-  }, 5000);
+  }, 1000);
 
-//const captain instead of let/var
-const captain = {//TODO: 'captain' is the [OBJECT] followed by its properties,
-    hull: 20,
-    firepower: 5,
-    accuracy: .7,
-    isAlive: true,//!current state
-    attack(target){
-        let ranNum = Math.random();//!Random number but 'window.crypto.getRandomValues()' is crypted for security
-        console.log(`%c Accuracy threshold is ${ranNum}`, `font-size:18px; background:darkgreen; color:white; border: 4px dashed gold;`);//template literal accessing variable
-        if (ranNum < this.accuracy) {//!'this' is directed @ captain's[object] accuracy
-            console.log(`Nice shot, Captan!`);
-            target.hull = target.hull - this.firepower;
-            console.log(`Alien has ${target.hull} hull points left.`);
-            if (target.hull <= 0) {
-                target.isAlive = false;
-                console.log(`You've saved us from those pesky Aliens, once again! Thank you, Captain!`);
-            }
-        } else {
-            console.log(`You Missed!`);
-        }
+//!OPPONENT CLASS OOP LAYOUT: For Captain's and Alien SpaceShips
+
+class Player {
+    constructor(name, hull, firepower, accuracy){
+        this.name = name;
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
     }
 }
 
-//?CAPTAIN CLASS OOP LAYOUT:
-//TODO: COMPLETE THIS SECTION
+//!Captain & Alien - CLASS OOP LAYOUT EXTENDS/INHERITENCE with keyword 'new'
 
-// class captain {
-//     constructor(name, hull, firepower, accuracy){
-//         this.name = name;
-//         this.hull = hull;
-//         this.firepower = firepower;
-//         this.accuracy = accuracy;
-//     }
+let Captain = new Player('captain', 20, 5, .7)
+console.log(Captain);
 
-// }
+//!Loop through alien spaceships, create array.
 
-//?ALIEN CLASS OOP LAYOUT EXTENDS/INHERITENCE:
-//TODO: COMPLETE THIS SECTION
+const alienArr = []
+let alienCount = 0
 
-// class alien extends captain{
-//     constructor(name){
-//         hull = Math.floor(Math.random() * 6) + 3,
-//         firepower = Math.floor(Math.random() * 4) + 2,
-//         accuracy = Math.floor(Math.random() * .8) + .6,
-//     }
-// }
+for(let i = 1; i <= 6; i++){
+    //TODO: Setup Alien Properties
+    //!Alien Hull @ random
+    let alienHull = Math.floor(Math.random() * 6) + 3;
+    //!Alien Firepower @ random
+    let alienFirePower = Math.floor(Math.random() * 4) + 2;
+    //!Alien Accuracy @ random
+    let alienAccuracy = Math.floor(Math.random() * .8) + .6;
+    //TODO: 'new' Alien i.e. Inheritance/Extends from Opponent object above
+    let Alien = new Player('alien ship', alienHull, alienFirePower, alienAccuracy)
+    //!Battle each alien one at a time from alienArr
+    alienArr.push(Alien);
+};
+console.log(alienArr);
 
-//! ALIEN SHIP BUILD
-//TODO: 'hull' -  between 3 and 6 
-//TODO: 'firepower' - between 2 and 4
-//TODO: 'accuracy' -  between .6 and .8
-const alien = {
-    hull: Math.floor(Math.random() * 6) + 3,//any random # between 3 & 6
-    firepower: Math.floor(Math.random() * 4) + 2,//any random # between 2 & 4
-    accuracy: Math.floor(Math.random() * .8) + .6,//update to make it to the hundredth?
-    isAlive: true,//current state, i.e. starting point
-    attack(target) {//attack function rolls out as follows
-        let ranNum = Math.random();
-        console.log(`Accuracy threshold is ${ranNum}`);
-        if (ranNum < this.accuracy) {
-            console.log(`You've been hit!`);
-            target.hull = target.hull - this.firepower;
-            console.log(`hero has ${target.hull} hull points left.`);
-            if (target.hull <= 0) {
-                target.isAlive = false;
-                console.log(`GAME OVER!`);
-            }
-        } else {
-            console.log(`Phew! That was close!`);
-        }
+//!Loop through length of Battle (Six Aliens) i.e. alienArr.length
+//TODO: WHILE LOOP
+
+while (alienArr.length > 0) {
+    let aliensLeft = alienArr.pop();//!removes the last element from alienArr and returns that element
+    if (Math.random() < aliensLeft.accuracy) {
+      console.log("%c Mayday Captain, we've been hit!", "font-weight: bold; font-size: 15px; font-variant: small-caps; color: red");
+      Captain.hull = Captain.hull - aliensLeft.firepower;
+      console.log(Captain.hull);
+    } else if (Math.random() > aliensLeft.accuracy) {
+      console.log("%c Great shot! " + Captain.firepower + " damage!", "font-style: italic; font-variant: small-caps; text-transform: capitalize; font-size: 15px; background: azure; border: 1px solid green");
+      console.log(aliensLeft.hull - Captain.firepower);
+    } console.log("%c Great Job Captain, Alien ship destroyed!", "font-weight: bold; font-variant: small-caps; font-size: 15px; font-variant: small-caps; color: blue");
+    //Checks the player's health (hull) to see it has reached 0 before continuing game
+    if (Captain.hull <= 0) {
+      console.log("%c We did our best captain!", "font-weight: bold; font-variant: small-caps; font-size: 15px; background: azure; border: 1px solid red; color: orange");
+      break;
+    }; //Confirm if the player wants to continue the same battle
+    if (!confirm("Continue: Yes/No?")) {
+      console.log("%c Coward, Why run from a winning battle!", "font-weight: bold; font-size: 15px; font-variant: small-caps; color: grey");
+      break;
     }
-}
-
-//! BATTLE BUILD - Let the games begin //
-const battle = (player, computer) => {//*These two params must be captain and alien
-    while (player.isAlive && computer.isAlive){//*while loop determines 'when captain or computer attacks'
-        player.attack(computer);
-        if (computer.isAlive) { 
-            computer.attack(player);
-        }
-    }
-}
-
-
-//!PLAYER
-    //!ATTACK
-//!VILLAIN
-    //!ATTACK
+  };
+    
+//Signals player the game is over if they won
+if (Captain.hull > 0 && alienArr.length == 0) {
+    console.log("%c You've saved us from those pesky Aliens! Thank you, Captain!", "background: azure; border: 1px solid grey; text-transform: capitalize; font-weight: bold; font-style: italic; font-variant: small-caps; color: indigo; font-size: 15px");
+  }
+  
 
 
 /**
@@ -144,3 +121,75 @@ const battle = (player, computer) => {//*These two params must be captain and al
  * TODO:
 */
 
+//======= WORKING THRU IT ======================================================================
+//const captain instead of let/var
+// const captain = {//TODO: 'captain' is the [OBJECT] followed by its properties,
+//     hull: 20,
+//     firepower: 5,
+//     accuracy: .7,
+//     isAlive: true,//!current state
+//     attack(target){//!the aliens
+//         let ranNum = Math.random();//!Random number but 'window.crypto.getRandomValues()' is crypted for security
+//         console.log(`%c Accuracy threshold is ${ranNum}`, `font-size:18px; background:darkgreen; color:white; border: 4px dashed gold;`);//template literal accessing variable
+//         if (ranNum < captain.accuracy) {//!'this' is directed @ captain's[object] accuracy
+//             console.log(`Nice shot, Captain!`);
+//             target.hull = target.hull - captain.firepower;
+//             console.log(`Alien has ${target.hull} hull points left.`);
+//             if (target.hull <= 0) {
+//                 target.isAlive = false;
+//                 console.log(`You've saved us from those pesky Aliens, once again! Thank you, Captain!`);
+//             }
+//         } else {
+//             console.log(`You Missed!`);
+//         }
+//     }
+// }
+//! ALIEN SHIP BUILD
+//TODO: 'hull' -  between 3 and 6 
+//TODO: 'firepower' - between 2 and 4
+//TODO: 'accuracy' -  between .6 and .8
+// const alien = {
+//     hull: Math.floor(Math.random() * 6) + 3,//any random # between 3 & 6
+//     firepower: Math.floor(Math.random() * 4) + 2,//any random # between 2 & 4
+//     accuracy: Math.floor(Math.random() * .8) + .6,//update to make it to the hundredth?
+//     isAlive: true,//current state, i.e. starting point
+//     attack(target) {//attack function rolls out as follows
+//         let ranNum = Math.random();
+//         console.log(`Accuracy threshold is ${ranNum}`);
+//         if (ranNum < alien.accuracy) {
+//             console.log(`You've been hit!`);
+//             target.hull = target.hull - alien.firepower;
+//             console.log(`hero has ${target.hull} hull points left.`);
+//             if (target.hull <= 0) {
+//                 target.isAlive = false;
+//                 console.log(`GAME OVER!`);
+//             }
+//         } else {
+//             console.log(`Phew! That was close!`);
+//         }
+//     }
+// }
+
+//!ALIENS
+//Define an array six Aliens
+// const alienArr = [];
+//Counting Aliens
+// var alienCount = 0;
+
+//! BATTLE BUILD - Let the games begin //
+// const battle = (player, computer) => {//*These two params must be captain and alien
+//     while (player.isAlive && computer.isAlive){//*while loop determines 'when captain or computer attacks'
+//         player.attack(computer);
+//         if (computer.isAlive) { 
+//             computer.attack(player);
+//         }
+//     }
+// }
+
+// class alien extends captain{
+//     constructor(name){
+//         hull = Math.floor(Math.random() * 6) + 3,
+//         firepower = Math.floor(Math.random() * 4) + 2,
+//         accuracy = Math.floor(Math.random() * .8) + .6,
+//     }
+// }
