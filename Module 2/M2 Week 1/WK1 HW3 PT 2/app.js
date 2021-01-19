b6cf90974041236b04cf0ba8d395d54fa4074a4a
Sara Peña <s.cristal.p@gmail.com>
2021-01-18 23:29:43 -0500

    Changes.js

--- Module 2/M2 Week 1/WK1 HW3 PT 2/app.js
+++ Module 2/M2 Week 1/WK1 HW3 PT 2/app.js
@@ -1,21 +1,17 @@
-ReactDOM.render(
-    <App />,
-    document.querySelector('.container')
-)
-
 class App extends React.Component {
     state = {
         baseURL: 'http://jservice.io/api/random',
         question:[],
         category: {
             title: ''},
-        toggleAnswer: false,
+        showAnswer: false,
+      
         score: 0
     }
     GetQuestion() {
           fetch(this.state.baseURL)
-            .then((response) => response.json())
-              //! returns in JSON
+            .then(response => {response.json()
+             }) //! returns in JSON
             .then(json => this.setState({
             question: json,
               //!clearing the field for new entry below,
@@ -26,8 +22,22 @@ class App extends React.Component {
     }
 
     toggleAnswer() {
-        this.setState({toggleAnswer: !this.state.toggleAnswer});
+        this.setState({showAnswer: !this.state.showAnswer});
     }
+
+// Points = (event) => {
+//     const currScore = this.state.score;
+//     const currValue = this.state.value;
+//     if(event.target.id === 'incScore'){
+//         currScore = currScore + currValue;
+//     }
+//     if(event.target.id === "decScore"){
+//         currScore = currScore - currValue;
+//     }
+//     this.setState(
+//         {score: currScore}
+//     )
+// }
 
     addScore() {
      const score = (this.state.question.filter(val => (val.value)));
@@ -47,7 +57,7 @@ class App extends React.Component {
 render () {
     //another way to write this: const {showHideAnswer} = this.state :is
       //const  showHideAnswer = this.state.showHideAnswer;
-      const {showHideAnswer} = this.state;
+      const {showAnswer} = this.state.showAnswer;
       const question = (this.state.question.map(question => (question.question)));
       const title = (this.state.question.map(title => (title.category.title)));
       const point = (this.state.question.map(point => (point.value)));
@@ -57,16 +67,21 @@ render () {
             <div>
             <h1>Welcome to Ydrapoej!</h1>
             <label className="score">Score: {this.state.score}</label>
-            <button className="decScore" onClick={() => this.decreaseScore()}>Decrease</button>
-            <button className="incScore" onClick={() => this.increaseScore()}>Increase</button>
+            <button className="decScore" onClick={() => this.subScore()}>Decrease</button>
+            <button className="incScore" onClick={() => this.addScore()}>Increase</button>
             <button className="reset" onClick={() => this.reset()}>Reset</button>
             <h2>Let's Play!</h2>
-            <button className="getQuestion" onClick={() => this.handleFetchQuestion()}>Get Question</button><br />
+            <button className="getQuestion" onClick={() => this.GetQuestion()}>Get Question</button><br />
             <div className="question">{question}</div><br />
-            <div><label className="category">Category: {title}</label></div><br />
+            <div><label className="category">Category: {category}</label></div><br />
             <div><label className="point">Point: {point}</label></div><br /><br />
-            <button className="togAnswer" onClick={() => this.showHideAnswer()}>Answer</button><br /><br />
+            <button className="togAnswer" onClick={() => this.showAnswer()}>Answer</button><br /><br />
             {showHideAnswer && (<div className="answer">{answer}</div> )}
             </div>
     )
 }
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('.container')
+)
